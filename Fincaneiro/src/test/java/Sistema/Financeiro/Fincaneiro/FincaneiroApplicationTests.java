98d0bc29703f3c479314bd81e4ac471e6a1dd2de
henrique <37087436+SkBolttz@users.noreply.github.com>
2025-09-18 22:58:01 -0300

    Projeto Inicial

---
+++ Fincaneiro/src/test/java/Sistema/Financeiro/Fincaneiro/FincaneiroApplicationTests.java
@@ -0,0 +1,13 @@
+package Sistema.Financeiro.Fincaneiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincaneiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
